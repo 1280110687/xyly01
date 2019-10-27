0c3142be36cea2c1544ccda470f9a9ecf524c80c
1280110687 <53083893+1280110687@users.noreply.github.com>
2019-10-27 13:59:01 +0800

    01-闲云旅游（初始版）

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-button type="info">信息按钮</el-button>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="less" scoped>
+div {
+  h1 {
+    background-color: #000;
+  }
+}
+</style>
