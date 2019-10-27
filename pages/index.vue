3db28cc1c4d7d3e2e670c0c186ef0cd624a63e71
1280110687 <53083893+1280110687@users.noreply.github.com>
2019-10-27 18:32:56 +0800

    03-公共部分搭建展示
    1，添加公共组件“头部”和“底部”

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,17 @@
 <template>
   <div>
-    <el-button type="info">信息按钮</el-button>
-    <nuxt-link to="/post">攻略</nuxt-link>
-    <nuxt-link to="/hotel">酒店</nuxt-link>
-    <nuxt-link to="/air">机票</nuxt-link>
-    <nuxt-link to="/user/login">登录</nuxt-link>
+    <h3>首页</h3>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
-    Logo
   }
 }
 </script>
 
 <style lang="less" scoped>
-div {
-  h1 {
-    background-color: #000;
-  }
-}
+
 </style>
