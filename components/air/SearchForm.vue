3c315c51444f5322ca167cf8e390e2cb142cdf11
1280110687 <53083893+1280110687@users.noreply.github.com>
2019-10-29 12:06:24 +0800

    10-机票列表布局初步实现

--- components/air/SearchForm.vue
+++ components/air/SearchForm.vue
@@ -11,13 +11,21 @@
     <div class="search_content">
       <el-form label-width="80px">
         <el-form-item label="出发城市">
-          <el-autocomplete :fetch-suggestions="querySearchAsync" v-model="form.departCity" @select="handleSelect1"></el-autocomplete>
+          <el-autocomplete
+            :fetch-suggestions="querySearchAsync"
+            v-model="form.departCity"
+            @select="handleSelect1"
+          ></el-autocomplete>
         </el-form-item>
         <!-- 换 -->
         <div class="city_change" @click="handleCityChange">换</div>
         <!-- 换 -->
         <el-form-item label="到达城市">
-          <el-autocomplete :fetch-suggestions="querySearchAsync" v-model="form.destCity" @select="handleSelect2"></el-autocomplete>
+          <el-autocomplete
+            :fetch-suggestions="querySearchAsync"
+            v-model="form.destCity"
+            @select="handleSelect2"
+          ></el-autocomplete>
         </el-form-item>
         <el-form-item label="出发时间">
           <el-date-picker
@@ -29,7 +37,7 @@
           ></el-date-picker>
         </el-form-item>
         <el-form-item>
-          <el-button style="width: 100%;" type="primary">搜索</el-button>
+          <el-button style="width: 100%;" type="primary" @click="handleGetTicket">搜索</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -76,17 +84,17 @@ export default {
       }
     },
     // 获取出发城市编码
-    handleSelect1(item){
+    handleSelect1 (item) {
       console.log(item)
       this.form.departCode = item.code
       console.log(item.code)
     },
     // 获取到达城市编码
-    handleSelect2(item){
+    handleSelect2 (item) {
       this.form.destCode = item.code
     },
     // 点击 “ 换 ”  交换城市和编码
-    handleCityChange(){
+    handleCityChange () {
       // 简单的 对象 深拷贝 有小弊端： 对象中如果有 属性 = 函数格式 导致 属性丢失！！！！ 
       // let form=JSON.parse(JSON.stringify(this.form));
       // this.form.departCity=form.destCity;
@@ -97,8 +105,13 @@ export default {
 
       // low-----------------------
       // es6 交换
-      [this.form.departCity,this.form.departCode,this.form.destCity,this.form.destCode]=
-      [this.form.destCity,this.form.destCode,this.form.departCity,this.form.departCode]
+      [this.form.departCity, this.form.departCode, this.form.destCity, this.form.destCode] =
+        [this.form.destCity, this.form.destCode, this.form.departCity, this.form.departCode]
+    },
+    // 点击搜索  带参跳
+    handleGetTicket(){
+      // 欠此处一个表单验证
+      this.$router.push({ path: '/air/flights', query: this.form })
     }
   }
 }
---
+++ pages/air/flights.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flights">
+    <!-- 正文 开始 -->
+    <div class="flights_main">
+      <!-- 筛选模块 开始 -->
+      <FlightsFliter/>
+      <!-- 筛选模块 结束 -->
+
+      <!-- 表单头部 开始 -->
+      <FlightsHead/>
+      <!-- 表单头部 结束 -->
+
+      <!-- 机票列表 开始 -->
+      <FlightsItem/>
+      <!-- 机票列表 结束 -->
+    </div>
+    <!-- 正文 结束 -->
+
+    <!-- 侧边栏 开始 -->
+    <div class="flights_aside">2</div>
+    <!-- 侧边栏 结束 -->
+  </div>
+</template>
+<script>
+import FlightsFliter from "@/components/air/FlightsFliter"
+import FlightsHead from "@/components/air/FlightsHead"
+import FlightsItem from "@/components/air/FlightsItem"
+export default {
+  components: {
+    FlightsFliter, FlightsHead, FlightsItem
+  }
+}
+</script>
+<style lang="less" scoped>
+.flights {
+  width: 1000px;
+  margin: 0 auto;
+  display: flex;
+  .flights_main {
+    flex: 5;
+  }
+  .flights_aside {
+    flex: 2;
+  }
+}
+</style>
