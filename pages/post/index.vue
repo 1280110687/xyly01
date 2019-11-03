d2d16f1b51cc2b7ec152c409cd59cf49f9f14185
1280110687 <53083893+1280110687@users.noreply.github.com>
2019-11-03 16:01:25 +0800

    第二次测试

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,13 +1,35 @@
 <template>
-  <div>
-    <h1>旅游攻略</h1>
+  <div class="post">
+    <div class="post_mian">
+    <!-- 旅游攻略左侧栏  开始-->
+    <div class="post_left">
+      <postLeft/>
+    </div>
+    <!-- 旅游攻略左侧栏  结束-->
+    <!-- 旅游攻略主体  开始 -->
+    <div class="post_content">
+
+    </div>
+    <!-- 旅游攻略主体  结束 -->
+    </div>
   </div>
 </template>
 <script>
+import postLeft from "@/components/post/postLeft"
 export default {
-
+components: {
+  postLeft
+}
 }
 </script>
 <style lang="less" scoped>
+.post {
+  .post_mian {
+    width: 1000px;
+    margin: 0 auto;
+    .post_left {
 
+    }
+  }
+}
 </style>
