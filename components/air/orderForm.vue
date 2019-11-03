a2dfbb9b425121a60e04bbb75b0aeabc923780cd
1280110687 <53083893+1280110687@users.noreply.github.com>
2019-11-03 14:56:48 +0800

    26-订单提交copy来的
    ！！！！由于该提交属于copy。so   后期需要自己看看

--- components/air/orderForm.vue
+++ components/air/orderForm.vue
@@ -242,41 +242,45 @@ export default {
       const token = this.$store.state.user.userinfo.token
       this.$axios
         .post('/airorders', form, {
-          headers: { Authorization: `Bearer ${token}`}
+          headers: { Authorization: `Bearer ${token}` }
         })
         .then(res => {
           console.log(res)
           this.$message.success("订单提交成功")
+          // 跳转路由   带上该订单航班信息
+          setTimeout(() => {
+            this.$router.push({ path: "/air/pay", query: { id: res.data.data.id } })
+          }, 1000)
         })
+    }
+  },
+  computed: {
+    // 使用计算属性计算订单总额
+    price () {
+      // 1 先声明
+      let price = 0;
+      // 机票费用
+      price += this.airTicket.base_price;
+      // 机建、燃油费
+      price += this.airTicket.airport_tax_audlet;
+      // 保险费  因为是复选框且是数组形式 So 要拿到 数组 id 去 ticket.insurances 找到 这个id 的价格
+      // !!!! 遍历的是data中的数字对象
+      this.insurances.forEach(v => {
+        // 遍历这个数组，再找到该索引对应的数据     vv =  {id: 1, type: "航空意外险", price: 30, compensation: "260万", created_at: 1555487082133,…}
+        const index = this.airTicket.insurances.findIndex(vv => vv.id === v);
+        const item = this.airTicket.insurances[index];
+
+        price += item.price;
+      })
+      price *= this.users.length;
+
+      // 给父组件传值
+      let usersLength = this.users.length
+      this.$emit('countPrice', price, usersLength)
+
+      return price
+    }
   }
-},
-computed: {
-  // 使用计算属性计算订单总额
-  price() {
-    // 1 先声明
-    let price = 0;
-    // 机票费用
-    price += this.airTicket.base_price;
-    // 机建、燃油费
-    price += this.airTicket.airport_tax_audlet;
-    // 保险费  因为是复选框且是数组形式 So 要拿到 数组 id 去 ticket.insurances 找到 这个id 的价格
-    // !!!! 遍历的是data中的数字对象
-    this.insurances.forEach(v => {
-      // 遍历这个数组，再找到该索引对应的数据     vv =  {id: 1, type: "航空意外险", price: 30, compensation: "260万", created_at: 1555487082133,…}
-      const index = this.airTicket.insurances.findIndex(vv => vv.id === v);
-      const item = this.airTicket.insurances[index];
-
-      price += item.price;
-    })
-    price *= this.users.length;
-
-    // 给父组件传值
-    let usersLength = this.users.length
-    this.$emit('countPrice', price, usersLength)
-
-    return price
-  }
-}
 }
 </script>
 
