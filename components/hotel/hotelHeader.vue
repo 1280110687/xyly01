d9e9eb9c724ff4d31d7985147740f6526e563f7e
1280110687 <53083893+1280110687@users.noreply.github.com>
2019-11-05 11:27:23 +0800

    评论的动态渲染完成

---
+++ components/hotel/hotelHeader.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hotelHeader">
+    <div class="header_main">
+      <!-- 面包屑 -->
+      <div class="header_bum">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/hotel' }">酒店</el-breadcrumb-item>
+          <el-breadcrumb-item>xxx酒店预订</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <!-- 酒店筛选  begin -->
+      <div class="header_flights">
+        <div class="flights_city">
+          <el-autocomplete :fetch-suggestions="querySearchAsync" placeholder="请输入内容"></el-autocomplete>
+        </div>
+        <div class="flights_time">
+          <el-date-picker
+            v-model="value6"
+            type="daterange"
+            range-separator="-"
+            start-placeholder="入住日期"
+            end-placeholder="离店日期"
+          ></el-date-picker>
+        </div>
+        <div class="flights_person">
+          <el-input placeholder="人数" suffix-icon="el-input__icon iconfont iconuser" v-model="input2"></el-input>
+        </div>
+        .
+      </div>
+      <!-- 酒店筛选  end -->
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="less" scoped>
+.hotelHeader {
+  .header_main {
+    .header_bum {
+    }
+    .header_flights {
+      display: flex;
+      .flights_city {
+
+      }
+      .flights_time {
+        margin: 0 10px 0 20px;
+      }
+      .flights_person {
+
+      }
+    }
+  }
+}
+</style>
--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>
-    酒店
+  <div class="hotel">
+    <div class="hotel_main">
+      <!-- 头部 -- 面包屑和筛选 -->
+      <hotelHeader />
+    </div>
   </div>
 </template>
 <script>
+import hotelHeader from "@/components/hotel/hotelHeader"
 export default {
-
+  components: {
+    hotelHeader
+  }
 }
 </script>
 <style lang="less" scoped>
-
+.hotel {
+  .hotel_main {
+    max-width: 1000px;
+    margin: 0 auto;
+  }
+}
 </style>
